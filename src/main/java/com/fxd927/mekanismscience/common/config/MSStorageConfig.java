45e8e11d9ecf58ed0900f947230b54eb8dd364fa
kotak <fixdol1127@gmail.com>
2024-10-02 15:55:36 +0900

    Added AbsorptionTypeSeawaterMetalExtractor

--- src/main/java/com/fxd927/mekanismscience/common/config/MSStorageConfig.java
+++ src/main/java/com/fxd927/mekanismscience/common/config/MSStorageConfig.java
@@ -9,12 +9,16 @@ import net.minecraftforge.fml.config.ModConfig;
 public class MSStorageConfig extends BaseMekanismConfig {
     private final ForgeConfigSpec configSpec;
 
+    public final CachedFloatingLongValue adsorptionTypeSeawaterMetalExtractor;
     public final CachedFloatingLongValue seawaterPump;
     public final CachedFloatingLongValue organicLiquidExtractor;
 
     MSStorageConfig() {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.comment("Science Energy Storage Config. This config is synced from server to client.").push("storage");
+
+        adsorptionTypeSeawaterMetalExtractor = CachedFloatingLongValue.define(this, builder, "Base energy storage (Joules).", "adsorptionTypeSeawaterMetalExtractor",
+                FloatingLong.createConst(40_000));
 
         organicLiquidExtractor = CachedFloatingLongValue.define(this, builder, "Base energy storage (Joules).", "organicLiquidExtractor",
                 FloatingLong.createConst(40_000));
